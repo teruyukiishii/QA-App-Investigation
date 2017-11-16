9e005582c7879412cdfb5fbea861f1cd52889e66
TeruyukiIshii <TeruyukiIshii@gmail.com>
2017-11-17 00:03:21 +0900

    facebook auth

--- src/App.vue
+++ src/App.vue
@@ -4,11 +4,15 @@
         <div class="center">{{ title }}</div>
         <div class="right">
           <v-ons-toolbar-button>
-            <v-ons-icon icon="ion-navicon, material: md-menu"></v-ons-icon>
+            <v-ons-icon icon="md-menu"></v-ons-icon>
           </v-ons-toolbar-button>
+          <v-ons-button @click="login">
+            <v-ons-icon icon="md-facebook"></v-ons-icon> 
+          </v-ons-button>
+          {{ name }} 
         </div>
       </v-ons-toolbar>
-      <div style="text-align: center; padding-top:10px">Hello World!</div>
+
       <v-ons-tabbar swipeable position="auto"
         :tabs="tabs"
         :visible="true"
@@ -27,6 +31,7 @@
     data() {
       return {
         activeIndex: 0,
+        userName: '',
         tabs: [
           {
             icon: 'md-home',
@@ -51,10 +56,32 @@
         ]
       };
     },
+    methods: {
+      login() {
+        var ref = window.open('https://rocky-scrubland-17659.herokuapp.com/auth/facebook?auth_origin_url=http://titeck.html.xdomain.jp/&omniauth_window_type=newWindow', "_blank", "location=yes");
+
+        var messanger = setInterval(function() {
+          var message = 'requestCredentials';
+          ref.postMessage(message, 'https://rocky-scrubland-17659.herokuapp.com/');
+        }, 500);
+      },
+      afterLogin(rec) {
+        this.userName = rec.data['name']; 
+      }
+    },
+    created() {
+      window.addEventListener('message', this.afterLogin);
+    },
+    destroyed() {
+      window.removeEventListener('message', this.afterLogin);
+    },
     computed: {
       title() {
         return this.tabs[this.activeIndex].label; 
-      } 
+      },
+      name() {
+        return this.userName;
+      }
     }
   };
 </script>
